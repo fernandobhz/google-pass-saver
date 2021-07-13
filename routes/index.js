99d564ec3b218bd890379fc3d8ef0e3432cb5ccd
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-07-12 22:21:52 -0300

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+const title = 'Googler Pass Saver';
+const user = "";
+const pass = "";
+
+router.get('/', function(req, res, next) {
+  res.redirect('/TheAppName');
+});
+
+router.get('/:app', function(req, res, next) {
+  res.render('index', { title, user, pass });
+});
+
+router.post('/:app', function(req, res, next) {
+  const { user, pass } = req.body;
+  res.render('index', { title, user, pass });
+})
+
+module.exports = router;
